77fe1fe9c23c01e5a1127fbea33e0bba92a7f9d9
itsbelledivine <47565460+itsbelledivine@users.noreply.github.com>
2019-03-19 19:28:18 +0000

    update 2

--- groupindex.js
+++ groupindex.js
@@ -1,4 +1,4 @@
-body window.onload = function() {
+window.onload = function() {
     //get the canvas and conext and store in vars
     var canvas = document.getElementById("sky");
     var ctx = canvas.getContext("2d");
